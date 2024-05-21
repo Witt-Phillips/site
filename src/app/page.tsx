10ab0f479bd9292bfa41a0e62b06ecd83ac789c3
Witt Phillips <witt.phillips@yale.edu>
2024-05-21 00:11:03 -0400

    Coming Soon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold">Welcome to My Homepage</h1>
+      <h1 className="text-4xl font-bold">Coming Soon</h1>
       <p className="text-lg mt-4 text-center">
         &ldquo;bababadalgharaghtakamminarronnkonnbronntonnerronntuonnthunntrovarrhounawnskawntoohoohoordenenthurnuk!&rdquo;
         <br />
