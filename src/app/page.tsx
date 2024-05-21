3174cf4f57b6adda66b1c9ee6a7801f5dfcac985
Witt Phillips <witt.phillips@yale.edu>
2024-05-21 00:09:51 -0400

    fixed escapes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+// app/page.tsx
 import React from 'react';
 
 const Home = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
-      <h1 className="text-4xl font-bold">Coming Soon</h1>
-      <p className="text-lg mt-4 text-center">“bababadalgharaghtakamminarronnkonnbronntonnerronntuonnthunntrovarrhounawnskawntoohoohoordenenthurnuk!” <br /> -James Joyce, <i>Finnegan's Wake</i></p>
+      <h1 className="text-4xl font-bold">Welcome to My Homepage</h1>
+      <p className="text-lg mt-4 text-center">
+        &ldquo;bababadalgharaghtakamminarronnkonnbronntonnerronntuonnthunntrovarrhounawnskawntoohoohoordenenthurnuk!&rdquo;
+        <br />
+        - James Joyce, <i>Finnegans Wake</i>
+      </p>
     </div>
   );
 };
